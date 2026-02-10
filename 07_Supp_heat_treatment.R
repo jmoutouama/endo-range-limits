bb11373b82a935d58345eb6f9f8892a7088df9f8
Tom Miller <tom.miller@rice.edu>
2026-02-10 12:19:09 -0600

    source population table

--- 07_Supp_heat_treatment.R
+++ 07_Supp_heat_treatment.R
@@ -2,19 +2,60 @@
 ## Author: Tom Miller
 
 library(tidyverse)
+library(magrittr)
 library(readxl)
+library(xtable)
 setwd("C:/Users/tm9/Dropbox/github/endo-range-limits")
 
 ##read in original plant data (we should include these as csv's in the repo)
 datini <- read.csv("https://www.dropbox.com/scl/fi/b93bvocqltadc36xirak2/Initialdata.csv?rlkey=8hd3z4th35lqvtfvam83kb972&dl=1", stringsAsFactors = F)
+##combine SHS and HUNT pops
+datini %<>% mutate(Population = recode_factor(Population, SHS = "HUNT"))
+
+##read in source info
+sources<-read.csv("Data/endo_range_limits_experiment- Source Population.csv")
+## drop SHS since it's a duplicate row with HUNT
+sources %<>% filter(Population!="SHS")
+## customize notes for ms table
+sources[1,c("Population","Notes")]
+sources$msNotes[1]<-"Private property, San Jacinto County, TX"
+sources[2,c("Population","Notes")]
+sources$msNotes[2]<-"Roadside collection, Columbus, TX"
+sources[3,c("Population","Notes")]
+sources$msNotes[3]<-"Stengl Lost Pines Field Station, Bastrop, TX"
+sources[4,c("Population","Notes")]
+sources$msNotes[4]<-"Roadside population, DeRidder, LA"
+sources[5,c("Population","Notes")]
+sources$msNotes[5]<-"Walter P. Jacobs Nature Center, Shreveport, LA"
+sources[6,c("Population","Notes")]
+sources$msNotes[6]<-"Kistachie National Forest, LA"
+sources[7,c("Population","Notes")]
+sources$msNotes[7]<-"Stephen F. Austin Experimental Forest, TX"
+sources[8,c("Population","Notes")]
+sources$msNotes[8]<-"Sam Houston National Forest, TX"
+sources[9,c("Population","Notes")]
+sources$msNotes[9]<-"Pineywoods Environmental Research Laboratory, Huntsville, TX"
+sources[10,c("Population","Notes")]
+sources$msNotes[10]<-"Llano River Field Station, Junction, TX"
+sources[11,c("Population","Notes")]
+sources$msNotes[11]<-"Palmetto State Park, TX"
+sources[12,c("Population","Notes")]
+sources$msNotes[12]<-"Jean Laffitte Park, LA"
+##update species names
+sources %<>% 
+  mutate(Species2 = fct_recode(factor(Species),
+                          "A. hyemalis" = "AGHY",
+                          "E. virginicus" = "ELVI",
+                          "P. autumnalis" = "POAU"))
 
 ##read in greenhouse inventory (all the plants we reared -- those in the experiment and many others)
 greenhouse<-read_excel("Data/2022-23_Greenhouse_Inventory.xlsx",sheet="Range Limit Greenhouse")
 str(greenhouse)
-
 #why is real_endo a character vector?
 unique(greenhouse$real_endo) ##because the excel function populates with "NA"
-
+##combine SHS and HUNT pops
+greenhouse %<>% mutate(POPULATION = recode_factor(POPULATION, SHS = "HUNT"))
+  
 ##how many genotypes from each soure population were used?
 source_tab1<-datini %>% 
   group_by(Species,Population) %>% 
@@ -28,11 +69,29 @@ source_tab2<-datini %>%
   summarise(mean_clones=mean(clones),min_clones=min(clones),max_clones=max(clones))
 
 ## combine into table for manuscript
-left_join(source_tab1,source_tab2,by=c("Species","Population"))
+supp_table_sources<-left_join(sources %>% filter(Population!="WB"),#no WB in the experiment
+          left_join(source_tab1,source_tab2,by=c("Species","Population")),
+          by=c("Species","Population")) %>% 
+  select(Species2,Population,Lat,Long,msNotes,number_plants,number_genotypes,mean_clones) %>% 
+  rename(
+    Species = Species2,
+    Population = Population,
+    Latitude = Lat,
+    Longitude = Long,
+    Notes = msNotes,
+    `Total plants` = number_plants,
+    `Unique genotypes` = number_genotypes,
+    `Mean clones per genotype` = mean_clones) %>% 
+  mutate(Species = paste0("\\textit{", Species, "}"))
+## format latex table
+tab<-xtable(supp_table_sources,digits = c(0,0,0,6,6,0,0,0,2))
+align(tab)<-"lllrrp{0.2\\textwidth}p{2cm}p{2cm}p{2cm}" ## wrap notes
+print(tab,file = "Manuscript/source_table.tex",include.rownames=F,
+  sanitize.text.function=identity,floating=F)
 
 ##filter the source pops in the greenhouse inventory 
 ##to only include those used in the experiment
-greenhouse %>% 
+greenhouse %>% %>%
   filter(POPULATION %in% datini$Population) %>% 
   #filter only plants with scores
   filter(!is.na(Leaf_peel_liberal)|!is.na(Leaf_peel_conservative)|
@@ -62,10 +121,10 @@ greenhouse %>% filter(`INDIVIDUAL ID` %in% problem_genotypes$`INDIVIDUAL ID`) %>
 
 ## reduce database to genotypes, collapse clones
 scored_greenhouse %>% 
-  select(SPECIES,POPULATION,`INDIVIDUAL ID`,
+  select(SPECIES,POPULATION,`INDIVIDUAL ID`,ENDOPHYTE,
          Leaf_peel_liberal,Leaf_peel_conservative,seed_score_liberal,
          seed_score_conservative,agrinostics_score_liberal,agrinostics_score_conservative) %>% 
-  group_by(SPECIES,POPULATION,`INDIVIDUAL ID`) %>% 
+  group_by(SPECIES,POPULATION,`INDIVIDUAL ID`,ENDOPHYTE) %>% 
   distinct() %>% 
   ##add indicator of which score type they had
   mutate(leaf_scored=!is.na(Leaf_peel_liberal) | !is.na(Leaf_peel_conservative),
@@ -77,10 +136,28 @@ scored_greenhouse %>%
 scored_greenhouse_genotypes %>% 
 ##subset to scores from multiple methods
   filter((leaf_scored+seed_scored+tiller_scored)>1) %>% 
-  mutate(mean_endo=)
+  rowwise() %>%
+  mutate(mean_liberal=mean(c_across(c(Leaf_peel_liberal,seed_score_liberal,agrinostics_score_liberal)),na.rm=T),
+         mean_conservative=mean(c_across(c(Leaf_peel_conservative,seed_score_conservative,agrinostics_score_conservative)),na.rm=T)) %>% 
+  ##find mismatrches (mean is not zero or one)
+  mutate(mismatch_liberal=mean_liberal!=0 & mean_liberal!=1,
+         mismatch_conservative=mean_conservative!=0 & mean_conservative!=1) %>% 
+  group_by() %>% 
+  summarise(n(),
+            mean(mismatch_liberal==0),
+            mean(mismatch_conservative==0))
 
-## calculate endo prevalence
+## calculate endo prevalence conditional on expected status
+scored_greenhouse_genotypes %>% 
+  ## I will use max() here so that in the few cases where scores differed, 
+  ## we will assume the positive score is correct
+  mutate(mean_liberal=max(c_across(c(Leaf_peel_liberal,seed_score_liberal,agrinostics_score_liberal)),na.rm=T),
+         mean_conservative=max(c_across(c(Leaf_peel_conservative,seed_score_conservative,agrinostics_score_conservative)),na.rm=T)) %>%
+  group_by(SPECIES,POPULATION,ENDOPHYTE) %>% 
+  summarise(count=n(),
+            prevalence_liberal=mean(mean_liberal),
+            prevalence_conservative=mean(mean_conservative)) %>% View
 
   
-  
-
+scored_greenhouse_genotypes %>% 
+  recode_factor(POPULATION,SHS="HUNT")
